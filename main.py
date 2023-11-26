22d014e7252ca67b74eaafb4ce5febdbabd22d5f
katushapvlva <katepav2001@mail.ru>
2023-11-26 16:48:19 +0300

    final fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from geopy.geocoders import Nominatim
+import geocoder
 import sqlite3
 import argparse
 from datetime import datetime
@@ -17,9 +17,9 @@ def get_weather_by_city(city):
         print_weather_info(city, weather_data)
         save_to_database((datetime.now(), city, weather_data["weather"][0]["description"], weather_data["main"]["temp"]))
     except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
+        print(f"HTTP error occurred, попробуйте снова")
     except requests.exceptions.RequestException as err:
-        print(f"Request exception occurred: {err}")
+        print(f"Request exception occurred, попробуйте снова")
 
 # Функция для вывода информации о погоде
 def print_weather_info(location, weather_data):
@@ -29,30 +29,27 @@ def print_weather_info(location, weather_data):
     print("Текущая температура:", weather_data["main"]["temp"], "градусов по Цельсию")
     print("Ощущается как:", weather_data["main"]["feels_like"], "градусов по Цельсию")
     print("Скорость ветра:", weather_data["wind"]["speed"], "м/c")
-    # Добавим информацию о других параметрах погоды при необходимости
-    # ...
 
 # Функция для получения информации о погоде по текущему местоположению
 def get_weather_by_location():
     try:
-        geolocator = Nominatim(user_agent="weather_app")
-        location = geolocator.geocode("me")
+        location = geocoder.ip("me")
 
         if location:
-            latitude, longitude = location.latitude, location.longitude
+            latitude, longitude = location.latlng
             url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=metric"
             response = requests.get(url)
             response.raise_for_status()  # Проверяем наличие ошибок при запросе
             weather_data = response.json()
-
+           
             print_weather_info(location.address, weather_data)
             save_to_database((datetime.now(), location.address, weather_data["weather"][0]["description"], weather_data["main"]["temp"]))
         else:
             print("Не удалось определить текущее местоположение")
     except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
+        print(f"HTTP error occurred, попробуйте снова")
     except requests.exceptions.RequestException as err:
-        print(f"Request exception occurred: {err}")
+        print(f"Request exception occurred, попробуйте снова")
 
 # Функция для сохранения результатов запросов в базу данных
 def save_to_database(data):
@@ -77,25 +74,28 @@ def save_to_database(data):
 
 # Функция для вывода последних результатов запросов из базы данных
 def print_history(n):
-    try:
-        connection = sqlite3.connect("weather.db")
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT * FROM weather_requests ORDER BY timestamp DESC LIMIT {n}")
-        results = cursor.fetchall()
-        connection.close()
+    if n < 0:
+        print("Введите значение n > 0")
+    else:
+        try:
+            connection = sqlite3.connect("weather.db")
+            cursor = connection.cursor()
+            cursor.execute(f"SELECT * FROM weather_requests ORDER BY timestamp DESC LIMIT {n}")
+            results = cursor.fetchall()
+            connection.close()
 
-        if results:
-            print("Последние", n, "результатов запросов:")
-            for result in results:
-                print("Время запроса:", result[0])
-                print("Место:", result[1])
-                print("Погодные условия:", result[2])
-                print("Температура:", result[3])
-                print("="*20)
-        else:
-            print("История запросов пуста.")
-    except sqlite3.Error as error:
-        print("Ошибка при работе с базой данных:", error)
+            if results:
+                print("Последние", n, "запросов:")
+                for result in results:
+                    print("Время запроса:", result[1])
+                    print("Название города:", result[2])
+                    print("Погодные условия:", result[3])
+                    print("Температура:", result[4])
+                    print("="*20)
+            else:
+                print("История запросов пуста.")
+        except sqlite3.Error as error:
+            print("Ошибка при работе с базой данных:", error)
 
 def main():
     parser = argparse.ArgumentParser()
